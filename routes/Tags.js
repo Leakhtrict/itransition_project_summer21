d195d57a65f2250ef9eafcfa17ec196a6419f5c9
agenis <artyomgenis@gmail.com>
2021-08-24 16:54:29 +0300

    deployment bug fixes 4

--- routes/Tags.js
+++ routes/Tags.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { Tags } = require("../models");
-const { validateToken } = require("../middlewares/AuthMiddleware");
 
 router.get("/", async (req, res) => {
     const listOfTags = await Tags.findAll();
@@ -10,12 +9,12 @@ router.get("/", async (req, res) => {
 
 router.post("/addTags", (req, res) => {
     const selectedTags = req.body;
-    console.log(selectedTags);
     selectedTags.map( async (value, key) => {
         if(value.__isNew__){
             await Tags.create({ tagName: value.label})
         }
     });
+    res.json("added tags successfully");
 });
 
 module.exports = router;
